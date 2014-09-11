f4c6c8a45e0c3db6b6dbcd3781ffa37f12ac18fd
Joshua Fogg <joshua.h.fogg@gmail.com>
2014-09-11 17:43:27 +0100

    Removed legacy version

--- .wren/files/readme.md
+++
@@ -1,4 +0,0 @@
-[fogg.me.uk](http://fogg.me.uk)
-===================
-
-My website, previously found at foggalong.github.io and foggalong.comlu.com
--- .wren/files/tools/reading.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-from math import floor
-
-text = input("Paste text...\n").strip().split(" ")
-if len(text)/200 < 1:
-    print("~%d seconds reading" % (float(len(text))*0.3))
-else:
-    print("~%d minutes %d seconds reading" % (floor(len(text)/float(200)), ((len(text)/float(200))-floor(len(text)/float(200)))*60))
