cd781e29b631792e867cd8902ef4c2ad992e6cdd
Joshua Fogg <joshua.h.fogg@gmail.com>
2014-11-02 16:54:44 +0000

    Fixed PHP links

--- php/meta.php
+++ php/meta.php
@@ -1,8 +1,8 @@
 <meta name="description" content="Simple Personal Website"> 
-    <meta name="keywords" content="Blog, Programming"> 
-    <meta name="author" content="Joshua Fogg"> 
+    <meta name="keywords" content="Blog"> 
+    <meta name="author" content="BLOGGER"> 
     <meta charset="UTF-8"> 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="http://fogg.me.uk/style/main.css">
-    <link rel="icon" href="http://fogg.me.uk/images/icons/flav.ico">
-    <link rel="shortcut icon" href="http://fogg.me.uk/images/icons/flav.ico">
+    <link rel="stylesheet" type="text/css" href="http://BLOGURL/style/main.css">
+    <link rel="icon" href="http://BLOGURL/images/icons/flav.ico">
+    <link rel="shortcut icon" href="http://BLOGURL/images/icons/flav.ico">
--- php/navbar.php
+++ php/navbar.php
@@ -1,23 +1,23 @@
 <div class="nav">
     <ul class="nav">
         <li class="nav" title="Home">
-            <a class="nav" href="http://fogg.me.uk/">
-                <img class="image" src="http://fogg.me.uk/images/nav/home.svg" alt="Home">
+            <a class="nav" href="http://BLOGURL/">
+                <img class="image" src="http://BLOGURL/images/nav/home.svg" alt="Home">
             </a>
         </li>
         <li class="nav" title="Blog">
-            <a class="nav" href="http://fogg.me.uk/blog">
-                <img class="image" src="http://fogg.me.uk/images/nav/blog.svg" alt="Blog">
+            <a class="nav" href="http://BLOGURL/blog">
+                <img class="image" src="http://BLOGURL/images/nav/blog.svg" alt="Blog">
             </a>
         </li>
         <li class="nav" title="About">
-            <a class="nav" href="http://fogg.me.uk/about">
-                <img class="image" src="http://fogg.me.uk/images/nav/about.svg" alt="About">
+            <a class="nav" href="http://BLOGURL/about">
+                <img class="image" src="http://BLOGURL/images/nav/about.svg" alt="About">
             </a>
         </li>
         <li class="nav-r" title="Top">
             <a class="nav" href="#">
-                <img class="image" src="http://fogg.me.uk/images/nav/top.svg" alt="Top">
+                <img class="image" src="http://BLOGURL/images/nav/top.svg" alt="Top">
             </a>
         </li>
     </ul>
